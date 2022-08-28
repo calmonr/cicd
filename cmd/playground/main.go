6d4a5d3e2be6881ab2f94cb115366a57ae91444f
Calmon Ribeiro <calmonrib@gmail.com>
2022-08-27 23:24:28 -0300

    feat: this pull request must purposely fail

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+func fail() error {
+	return fmt.Errorf("purposely fail: %s", "ooops!")
+}
+
 func main() {
-	fmt.Println("Welcome to the jungle!")
+	if err := fail(); err != nil {
+		panic(err)
+	}
 }
