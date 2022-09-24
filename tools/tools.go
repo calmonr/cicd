891db957bb317e053b129b9b1533f4a71e4af16a
Calmon Ribeiro <calmonrib@gmail.com>
2022-09-24 15:45:52 +0000

    ci: buf actions and adjustments

--- tools/tools.go
+++ tools/tools.go
@@ -4,6 +4,7 @@ package tools
 
 import (
 	_ "github.com/bufbuild/buf/cmd/buf"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/goreleaser/goreleaser"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
